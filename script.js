fc8b3529238ad19d9707ff24adfe7bac97eaeadd
Youbraj Singh <blazeplays2003@gmail.com>
2023-12-13 04:41:00 +0530

    added cursor element

--- script.js
+++ script.js
@@ -22,30 +22,63 @@ function videoCon() {
 
     videoCon.addEventListener("mousemove", (dets) => {
         gsap.to(playbtn, {
-            left: dets.x-60,
-            top: dets.y -60
+            left: dets.x - 60,
+            top: dets.y - 60
         })
     })
 }
 
 videoCon();
 
-function loadingAnimation(){
-    gsap.from("#page1 h1",{
+function loadingAnimation() {
+    gsap.from("#page1 h1", {
         y: 80,
         stagger: 0.3,
         opacity: 0,
         delay: 0.5,
-        duration:1
+        duration: 1
     });
-    gsap.from("#video-container video",{
+    gsap.from("#video-container video", {
         scale: 0.9,
         opacity: 0,
         delay: 0.5,
-        duration:1,
-        z:0
+        duration: 1,
+        z: 0
     })
 }
 
 loadingAnimation()
 
+document.addEventListener("mousemove", (dets) => {
+    gsap.to("#cursor", {
+        top: dets.y,
+        left: dets.x
+    })
+})
+
+// document.querySelectorAll(".child").addEventListener("mouseenter", () => {
+//     gsap.to("#cursor", {
+//         transform: "translate(-50%,-50%) scale(1)"
+//     })
+// })
+// document.querySelectorAll(".child").addEventListener("mouseleave", () => {
+//     gsap.to("#cursor", {
+//         transform: "translate(-50%,-50%) scale(0)"
+//     })
+// })
+
+
+
+let childs = document.querySelectorAll(".child");
+childs.forEach(element => {
+    element.addEventListener("mouseenter", () => {
+        gsap.to("#cursor", {
+            transform: "translate(-50%,-50%) scale(1)"
+        })
+    })
+    element.addEventListener("mouseleave", () => {
+        gsap.to("#cursor", {
+            transform: "translate(-50%,-50%) scale(0)"
+        })
+    })
+})
